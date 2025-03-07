eb1dc8899bae60ee49446d36fbf59ce7f9e3e0b5
SaraHarib <saraharib12@gmail.com>
2025-03-07 21:40:51 +0000

    Update Exercice4.cpp

--- Exercice4.cpp
+++ Exercice4.cpp
@@ -1,51 +1,50 @@
-#include <cstdlib>
 #include <iostream>
-
 using namespace std;
 
-int main(int argc, char *argv[])
+int KadaneAlgorithm(int T[3][4], int n, int m)
 {
-     int L, C;
-     cout << "Saisir le nombre de lignes : ";
-    cin >> L;
-    cout << "Saisir le nombre de colonnes : ";
-    cin >> C;
-    int tab[L][C];
-    for (int i = 0; i < L; i++) {
-        for (int j = 0; j < C; j++) {
-            cout << "Saisir le nombre de la ligne " << (i + 1) << " et la colonne " << (j + 1) << " : ";
-            cin >> tab[i][j];
+    int max_t = 0;
+    int pos = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int max_ending_here = 0;
+        int max_so_far = 0;
+        for (int j = 0; j < 4; j++)
+        {
+            max_ending_here = max_ending_here + T[i][j];
+            max_ending_here = max(max_ending_here, 0);
+            max_so_far = max(max_so_far, max_ending_here);
         }
+        if (max_t > max_so_far)
+        {
+            pos = i;
+        }
+        max_t = max(max_t, max_so_far);
     }
-    cout << endl;
-    for (int i = 0; i < L; i++) {
-        for (int j = 0; j < C; j++) {
-            cout << "  " << tab[i][j];
+    cout << "sous tableau maximal [";
+    for (int j = 0; j < m; j++)
+    {
+        cout << "\t" << T[pos][j];
+    }
+    cout << "]";
+
+    return max_t;
+}
+
+int main()
+{
+
+    int T[3][4] = {{-2, 1, -3, 4}, {-1, 2, 1, -5}, {4, -1, 2, 1}};
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << T[i][j] << " ";
         }
         cout << endl;
     }
-    int s[L] = {0}; 
-    for (int i = 0; i < L; i++) {
-        for (int j = 0; j < C; j++) {
-            s[i] += tab[i][j];
-        }
-    }
-    int m = 0;
-    for (int i = 0; i < L; i++) {
-        for (int j = 0; j < L; j++) {
-            if (s[i] > s[j]) {
-                m = i;
-            } else {
-                m = j;
-            }
-        }
-    }
-    cout << endl;
-    for (int i = 0; i < C; i++) {
-        cout << " " << tab[m][i];
-    }
-    cout << " = " << s[m] << endl;
 
+    int k = KadaneAlgorithm(T, 3, 4);
+    cout << "\t somme est :" << k;
     return 0;
-    
 }
